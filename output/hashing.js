540ad2b1356d05335eff2505bddb198be0366344
yeahieaKhan <yeahieakhan2017@gmail.com>
2026-02-10 05:57:22 +0600

    hassing

--- output/hashing.js
+++ output/hashing.js
@@ -1,1 +1,24 @@
-console.log("starting hashing");
+// start learning hashing
+
+const crypto = require("crypto");
+
+console.log("\n MD5 Hash : ");
+const md5Hash = crypto.createHash("md5").update("password124").digest("hex"); //not recommended
+const md5Hash2 = crypto.createHash("md5").update("password124").digest("hex"); //not recommended
+console.log("Input : password123");
+console.log("MD5 hasedPassword :", md5Hash);
+console.log("MD5 hashed 2:", md5Hash2);
+
+const sha256Hash = crypto
+  .createHash("sha256")
+  .update("password123")
+  .digest("hex");
+console.log("Input: password123");
+console.log("output", sha256Hash);
+
+const sha512Hash = crypto
+  .createHash("sha512")
+  .update("password123")
+  .digest("hex");
+console.log("Input: password123");
+console.log("output", sha512Hash);
